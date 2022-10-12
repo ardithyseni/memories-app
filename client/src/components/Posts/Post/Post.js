97c4bdd29a59a882588cfd367af675477368d981
ardithyseni <ah49393@ubt-uni.net>
2022-10-12 19:48:22 +0200

    likes now instantaneous in clientside

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, CardActions, CardContent, CardMedia, Button, Typography, ButtonBase } from '@mui/material';
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined';
@@ -18,13 +18,29 @@ const Post = ({ post, setCurrentId }) => {
 
     const user = JSON.parse(localStorage.getItem('profile'));
 
+    const userId = user?.result.sub || user?.result?._id;
+
+    const [likes, setLikes] = useState(post?.likes);
+
+    const hasLikedPost = post.likes.find((like) => like === userId);
+
+    const handleLikeClick = async () => {
+        dispatch(likePost(post._id));
+
+        if (hasLikedPost) {
+            setLikes(post.likes.filter((id) => id !== userId))
+        } else {
+            setLikes([...post.likes, userId]);
+        }
+    };
+
     const Likes = () => {
-        if (post.likes.length > 0) {
-            return post.likes.find((like) => like === (user?.result.sub || user?.result?._id))
+        if (likes.length > 0) {
+            return likes.find((like) => like === userId)
                 ? (
-                    <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}`}</>
+                    <><ThumbUpAltIcon fontSize="small" />&nbsp;{likes.length > 2 ? `You and ${likes.length - 1} others` : `${likes.length} like${likes.length > 1 ? 's' : ''}`}</>
                 ) : (
-                    <><ThumbUpAltOutlinedIcon fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
+                    <><ThumbUpAltOutlinedIcon fontSize="small" />&nbsp;{likes.length} {likes.length === 1 ? 'Like' : 'Likes'}</>
                 );
         }
 
@@ -81,7 +97,7 @@ const Post = ({ post, setCurrentId }) => {
                 </CardContent>
             </ButtonBase>
             <CardActions sx={{ padding: '0 16px 8px 16px', display: 'flex', justifyContent: 'space-between' }}>
-                <Button size="small" color="primary" disabled={!user?.result} onClick={() => { dispatch(likePost(post._id)) }}>
+                <Button size="small" color="primary" disabled={!user?.result} onClick={handleLikeClick}>
                     <Likes />
                 </Button>
                 {(user?.result?.sub === post?.creator || user?.result?._id === post?.creator) && (
