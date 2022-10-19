e674d21ffea6690a57c2911c1047da0ee1f7e81b
ardithyseni <ah49393@ubt-uni.net>
2022-10-19 13:45:51 +0200

    dirname fix?

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static("client/build"));
 
     app.get('*', function (req, res) {
-        const index = path.join(__dirname, '..', 'client','build', 'index.html');
+        const index = path.join(__dirname, '../client/build/index.html');
         res.sendFile(index);
       });
 }
