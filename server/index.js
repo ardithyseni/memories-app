d945648ee6bd7579ec8eb7e85a5d8983ed574388
ardithyseni <ah49393@ubt-uni.net>
2022-10-19 13:18:07 +0200

    dirname

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ import userRoutes from './routes/users.js';
 
 const app = express();
 
+const __dirname = path.resolve();
+
 dotenv.config({path: './server/.env'});
 
 
@@ -25,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static("client/build"));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+        res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
     });
 }
 
